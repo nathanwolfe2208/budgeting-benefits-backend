c0c9d0c1c7958379d2e306484a8cc1589239ba1d
Nathan Wolfe <nathanwolfe2208@gmail.com>
2024-09-24 14:10:01 -0400

    Adding User DTO and model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '2208',
+      username: 'nathan',
+      entities: [],
+      database: 'bbb',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
